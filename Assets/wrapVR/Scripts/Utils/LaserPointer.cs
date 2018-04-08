8ed1b1753465500a6004436b85835d7e7dd51606
mynameisjohn <mynameisjohnj@gmail.com>
2018-04-08 16:53:20 -0400

    Renaming the pointer script to LaserPointer

---
+++ Assets/wrapVR/Scripts/Utils/LaserPointer.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wrapVR
+{
+    // Straight pointer renderer
+    // Uses a line renderer to draw curves
+    // for the various input activation states
+    public class LaserPointer : MonoBehaviour
+    {
+        public VRRayCaster Source;
+        public Reticle Reticle;
+        public Transform FromTransform;
+        public bool DisableWhileGrabbing = true;
+        public EActivation Activation = EActivation.TRIGGER;
+
+        // Off renderer is a single line
+        public Color OffColor = Color.white;
+        public Material OffMaterial;
+        LineRenderer m_OffRenderer;
+        public float OffStartWidth = 0.005f;
+        public float OffEndWidth = 0f;
+        public float OffLength = 2f;
+
+        public Color OnColor;
+        public Material OnMaterial;
+        LineRenderer m_OnRendererColor;
+        LineRenderer m_OnRendererWhite;
+        public float OnWidth = 0.5f;
+        public int CapVerts = 8;
+        [Range(0, 1)]
+        public float CenterWidthRel = 0.25f;
+
+        public bool hasOff { get { return OffMaterial; } }
+        public bool hasOn { get { return OnMaterial; } }
+
+        protected bool isPointerActive
+        {
+            get
+            {
+                switch (Activation)
+                {
+                    case EActivation.NONE:
+                        return true;
+                    case EActivation.TOUCH:
+                        return Source.Input.GetTouchpadTouch();
+                    case EActivation.TOUCHPAD:
+                        return Source.Input.GetTouchpad();
+                    case EActivation.TRIGGER:
+                        return Source.Input.GetTrigger();
+                }
+                return false;
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            // Make sure we have a raycast source
+            Source = Util.DestroyEnsureComponent(gameObject, Source);
+
+            // Reticle should be a separate transform
+            if (Reticle == null)
+                Reticle = GetComponentInChildren<Reticle>();
+
+            else if (Reticle.gameObject == gameObject)
+            {
+                Debug.LogWarning("Warning: Laser Pointer Reticle shares transform with laser pointer");
+            }
+
+            if (FromTransform == null)
+                FromTransform = Source.transform;
+
+            if (hasOff)
+            {
+                m_OffRenderer = new GameObject("OffLine").AddComponent<LineRenderer>();
+                m_OffRenderer.startWidth = OffStartWidth;
+                m_OffRenderer.endWidth = OffEndWidth;
+                m_OffRenderer.startColor = OffColor;
+                m_OffRenderer.material = OffMaterial;
+                m_OffRenderer.endColor = new Color(OffColor.r, OffColor.g, OffColor.b, 0);
+                m_OffRenderer.useWorldSpace = true;
+                m_OffRenderer.enabled = false;
+            }
+
+            if (hasOn)
+            {
+                m_OnRendererColor = new GameObject("OnLineColor").AddComponent<LineRenderer>();
+                m_OnRendererWhite = new GameObject("OnLineWhite").AddComponent<LineRenderer>();
+
+                Vector3[] v3LinePositions = new Vector3[] { Vector3.zero, new Vector3(0, 0, 1) };
+                foreach (LineRenderer r in new LineRenderer[] { m_OnRendererColor, m_OnRendererWhite })
+                {
+                    r.numCapVertices = CapVerts;
+                    r.positionCount = v3LinePositions.Length;
+                    r.SetPositions(v3LinePositions);
+                    r.useWorldSpace = true;
+                    r.material = OnMaterial;
+                }
+
+                m_OnRendererColor.startWidth = OnWidth;
+                m_OnRendererColor.endWidth = OnWidth;
+                m_OnRendererColor.startColor = OnColor;
+                m_OnRendererColor.endColor = OnColor;
+                m_OnRendererWhite.startWidth = CenterWidthRel * OnWidth;
+                m_OnRendererWhite.endWidth = CenterWidthRel * OnWidth;
+                m_OnRendererWhite.startColor = Color.white;
+                m_OnRendererWhite.endColor = Color.white;
+                m_OnRendererColor.enabled = false;
+                m_OnRendererWhite.enabled = false;
+            }
+
+            foreach (LineRenderer r in new LineRenderer[] { m_OffRenderer, m_OnRendererColor, m_OnRendererWhite })
+            {
+                if (r)
+                {
+                    r.transform.SetParent(transform);
+                    r.transform.localPosition = Vector3.zero;
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Don't do anything if laser is disabled
+            if (VRCapabilityManager.isLaserDisabled)
+                return;
+
+            if (!Source.isActiveAndEnabled)
+            {
+                if (hasOff)
+                {
+                    m_OffRenderer.enabled = false;
+                }
+                if (hasOn)
+                {
+                    m_OnRendererColor.enabled = false;
+                    m_OnRendererWhite.enabled = false;
+                }
+                return;
+            }
+
+            // Set reticle position
+            if (Reticle)
+            {
+                if (Source.CurrentInteractible)
+                    Reticle.SetPosition(Source.CurrentHit);
+                else
+                    Reticle.ClearPosition();
+            }
+
+            if (!(DisableWhileGrabbing && Source.isGrabbing) && isPointerActive)
+            {
+                if (hasOff)
+                {
+                    m_OffRenderer.enabled = false;
+                }
+                if (hasOn)
+                {
+                    m_OnRendererColor.enabled = true;
+                    m_OnRendererWhite.enabled = true;
+
+                    foreach (LineRenderer r in new LineRenderer[] { m_OnRendererColor, m_OnRendererWhite })
+                    {
+                        r.SetPosition(0, FromTransform.position);
+                        r.SetPosition(1, Reticle.transform.position);
+                    }
+                }
+            }
+            else
+            {
+                if (hasOff)
+                {
+                    m_OffRenderer.enabled = true;
+                    Vector3 v3ReticleDir = (Reticle.transform.position - FromTransform.position).normalized;
+                    m_OffRenderer.SetPosition(0, FromTransform.position);
+                    m_OffRenderer.SetPosition(1, FromTransform.position + OffLength * v3ReticleDir);
+                }
+                if (hasOn)
+                {
+                    m_OnRendererColor.enabled = false;
+                    m_OnRendererWhite.enabled = false;
+                }
+            }
+        }
+    }
+}
