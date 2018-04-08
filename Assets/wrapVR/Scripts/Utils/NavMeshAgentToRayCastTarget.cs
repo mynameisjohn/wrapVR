807dcf90ffc641de90d70ed63a18690964615b46
mynameisjohn <mynameisjohnj@gmail.com>
2018-04-08 16:53:55 -0400

    adding a script to send a nav mesh agent toward the ray cast target

---
+++ Assets/wrapVR/Scripts/Utils/NavMeshAgentToRayCastTarget.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace wrapVR
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class NavMeshAgentToRayCastTarget : MonoBehaviour
+    {
+        public EActivation Activation;
+
+        public List<VRRayCaster> RayCasters;
+
+        private void Start()
+        {
+            Util.RemoveInvalidCasters(RayCasters);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // If our activation is down then apply a force to the RB toward its target
+            foreach (VRControllerRaycaster rc in RayCasters)
+            {
+                if (rc.IsActivationDown(Activation))
+                {
+                    if (rc.CurrentInteractible)
+                        GetComponent<NavMeshAgent>().SetDestination(rc.GetLastHitPosition());
+                }
+            }
+        }
+    }
+}
