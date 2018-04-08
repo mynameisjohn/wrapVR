031b120cdcb7733d6630371eb5291ec00dea0b8a
mynameisjohn <mynameisjohnj@gmail.com>
2018-04-08 16:52:19 -0400

    Adding KeepOnBounds option to the ring follower script

--- Assets/wrapVR/Scripts/Utils/SmoothFollowRing.cs
+++ Assets/wrapVR/Scripts/Utils/SmoothFollowRing.cs
@@ -10,13 +10,17 @@ namespace wrapVR
         [Range(0, 5)]
         public float FollowTime;
 
-        Vector3 vel;
+        [Tooltip("Should we stay on the boundary of the ring?")]
+        public bool KeepOnBounds = true;
+
+        Vector3 m_Vel;
+
         void Update()
         {
             if (Target)
             {
-                Vector3 v3Target = Target.ClosestPoint(transform.position);
-                transform.position = Vector3.SmoothDamp(transform.position, v3Target, ref vel, FollowTime);
+                Vector3 v3Target = Target.ClosestPoint(transform.position, KeepOnBounds, GetComponent<Collider>());
+                transform.position = Vector3.SmoothDamp(transform.position, v3Target, ref m_Vel, FollowTime);
             }
         }
     }
