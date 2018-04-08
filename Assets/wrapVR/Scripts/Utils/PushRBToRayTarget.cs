71d234242dff7caccc0c32edc359cf6fe9bf3fef
mynameisjohn <mynameisjohnj@gmail.com>
2018-04-08 16:54:29 -0400

    adding a script to send a rigid body toward the ray cast target. It's a little more janky than the navmesh one but it works

---
+++ Assets/wrapVR/Scripts/Utils/PushRBToRayTarget.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wrapVR
+{
+    // Allow a rigid body to be pushed toward a raycaster's target
+    [RequireComponent(typeof(Rigidbody))]
+    public class PushRBToRayTarget : MonoBehaviour
+    {
+        public float Speed = 5;
+        public EActivation Activation;
+
+        public List<VRRayCaster> RayCasters;
+
+        private void Start()
+        {
+            Util.RemoveInvalidCasters(RayCasters);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // If our activation is down then apply a force to the RB toward its target
+            foreach (VRControllerRaycaster rc in RayCasters)
+            {
+                if (rc.IsActivationDown(Activation))
+                {
+                    if (rc.CurrentInteractible)
+                        GetComponent<Rigidbody>().AddForce(Speed * (rc.GetLastHitPosition() - transform.position).normalized, ForceMode.Force);
+                }
+            }
+        }
+    }
+}
