43f12f062cad271b14e01ca640a7797e1c1f22cc
mynameisjohn <mynameisjohnj@gmail.com>
2018-04-20 21:07:48 -0400

    Checking in barebones entitlement initialization function to create an executable for oculus rift. Hopefully it will get fenced out properly... unfortunately this requires that the platform SDK exists in the project. Maybe there's a define along with that...

---
+++ Assets/wrapVR/Scripts/Utils/OculusRiftEntitlementSimple.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Oculus.Platform;
+
+public class OculusRiftEntitlementSimple : MonoBehaviour
+{
+    // eat your heart out zuck
+#if !UNITY_ANDROID && WRAPVR_OCULUS
+    void Awake()
+    {
+        try
+        {
+            Core.AsyncInitialize();
+            Entitlements.IsUserEntitledToApplication().OnComplete(GetEntitlementCallback);
+        }
+        catch (UnityException e)
+        {
+            Debug.LogError("Platform failed to initialize due to exception.");
+            Debug.LogException(e);
+            // Immediately quit the application.
+            UnityEngine.Application.Quit();
+        }
+    }
+
+    void GetEntitlementCallback(Message msg)
+    {
+        if (msg.IsError)
+        {
+            Debug.LogError("You are NOT entitled to use this app.");
+            UnityEngine.Application.Quit();
+        }
+        else
+        {
+            Debug.Log("You are entitled to use this app.");
+        }
+    }
+#endif
+}
