cfba24444a06045a4cdef2c310a3e499501a8cdd
John Joseph <mynameisjohnj@gmail.com>
2018-07-16 14:43:03 -0400

    Writing a script to manually find the OVR tracked remote children if they're null (although I've assigned them in the prefab as well.)

---
+++ Assets/wrapVR/Scripts/Utils/InitOVRTrackedRemote.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wrapVR
+{
+#if WRAPVR_OCULUS
+    [RequireComponent(typeof(OVRTrackedRemote))]
+#endif
+    public class InitOVRTrackedRemote : MonoBehaviour
+    {
+#if WRAPVR_OCULUS
+        // Use this for initialization
+        void Start()
+        {
+            // As of OVRSDK 1.2.7 these aren't initialized in code
+            OVRTrackedRemote remote = GetComponent<OVRTrackedRemote>();
+            if (remote.m_modelGearVrController == null)
+                remote.m_modelGearVrController = transform.Find("GearVrControllerModel").gameObject;
+            if (remote.m_modelOculusGoController == null)
+                remote.m_modelOculusGoController = transform.Find("OculusGoControllerModel").gameObject;
+        }
+#endif
+    }
+}
