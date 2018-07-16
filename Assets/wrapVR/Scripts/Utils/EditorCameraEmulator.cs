7aad1547487b99d512b2437325ec4c0a222d9455
John Joseph <mynameisjohnj@gmail.com>
2018-07-16 19:31:01 -0400

    Renaming the EditorEmulate script to EditorCameraEmulate and using only middle mouse click to engage / disengage capture.

---
+++ Assets/wrapVR/Scripts/Utils/EditorCameraEmulator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wrapVR
+{
+    public class EditorCameraEmulator : MonoBehaviour
+    {
+        public float _RotationSpeed = 2.0F;
+
+        float _pitch;
+        float _yaw;
+        bool _captureMouse = false;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(2))
+            {
+                _captureMouse = !_captureMouse;
+                if (_captureMouse)
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                }
+
+            }
+            if (_captureMouse)
+            {
+                _pitch += _RotationSpeed * Input.GetAxis("Mouse Y");
+                _yaw += _RotationSpeed * Input.GetAxis("Mouse X");
+
+                _pitch = Mathf.Clamp(_pitch, -90f, 90f);
+
+                _yaw %= 360f;
+
+                transform.eulerAngles = new Vector3(-_pitch, _yaw, 0f);
+            }
+        }
+    }
+}
