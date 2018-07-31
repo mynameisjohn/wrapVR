9fbdb9f034992557843dcb4cba90fd375002c3a6
John Joseph <mynameisjohnj@gmail.com>
2018-07-31 16:55:49 -0400

    Adding a new script to test various SDK inputs / interactions with VR interactive items.

---
+++ Assets/wrapVR/Scripts/Utils/TestVRInteractiveItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wrapVR
+{
+    [RequireComponent(typeof(VRInteractiveItem))]
+    public class TestVRInteractiveItem : MonoBehaviour
+    {
+        public List<EActivation> ActivationsToTest;
+
+        // Use this for initialization
+        void Start()
+        {
+            foreach(EActivation activation in ActivationsToTest)
+            {
+                GetComponent<VRInteractiveItem>().ActivationDownCallback(activation, (VRRayCaster rc) => { Debug.Log(rc.name + ", " + activation + ", Down"); });
+                GetComponent<VRInteractiveItem>().ActivationUpCallback(activation, (VRRayCaster rc) => { Debug.Log(rc.name + ", " + activation + ", Up"); });
+                GetComponent<VRInteractiveItem>().ActivationOverCallback(activation, (VRRayCaster rc) => { Debug.Log(rc.name + ", " + activation + ", Over"); });
+                GetComponent<VRInteractiveItem>().ActivationOutCallback(activation, (VRRayCaster rc) => { Debug.Log(rc.name + ", " + activation + ", Out"); });
+            }
+        }
+    }
+}
