a33b56db507b21eb25430a13df6fec07f8b22257
John Joseph <mynameisjohnj@gmail.com>
2018-09-10 23:07:01 -0400

    When following a ring target with a height, make sure that the target point is a least RingTarget.Height above the ground. This came  up when following something going down a hill - the object's ring would be partially inside the hill. A different (better?) way would be to orient the target ring relative to the GameObject's surface normal, but this was easier...

--- Assets/wrapVR/Scripts/Utils/SmoothFollowRing.cs
+++ Assets/wrapVR/Scripts/Utils/SmoothFollowRing.cs
@@ -20,6 +20,12 @@ namespace wrapVR
             if (Target)
             {
                 Vector3 v3Target = Target.ClosestPoint(transform.position, KeepOnBounds, GetComponent<Collider>());
+                RaycastHit hitInfo = new RaycastHit();
+                if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, Target.Height))
+                {
+                    v3Target.y += Target.Height - hitInfo.distance;
+                }
+
                 transform.position = Vector3.SmoothDamp(transform.position, v3Target, ref m_Vel, FollowTime);
             }
         }
