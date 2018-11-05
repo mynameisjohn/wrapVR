7c8f87cb7c9deb89c1804243286380b4f51e6026
John Joseph <mynameisjohnj@gmail.com>
2018-11-05 01:18:28 -0500

    Updating smooth follow ring to a) allow XZ only following b) optionalize the collider (I was getitng some NaNs when there was a collider present... should probably investigate that.)

--- Assets/wrapVR/Scripts/Utils/SmoothFollowRing.cs
+++ Assets/wrapVR/Scripts/Utils/SmoothFollowRing.cs
@@ -6,6 +6,9 @@ namespace wrapVR
 {
     public class SmoothFollowRing : MonoBehaviour
     {
+        public bool XZOnly = false;
+        public bool UseColliderIfPresent = true;
+
         public RingCollider Target;
         [Range(0, 90)]
         public float FollowTime;
@@ -14,16 +17,28 @@ namespace wrapVR
         public bool KeepOnBounds = true;
 
         Vector3 m_Vel;
+        Collider m_LocalCollider;
+
+        private void Start()
+        {
+            if (UseColliderIfPresent)
+                m_LocalCollider = GetComponent<Collider>();
+        }
 
         void Update()
         {
             if (Target)
             {
-                Vector3 v3Target = Target.ClosestPoint(transform.position, KeepOnBounds, GetComponent<Collider>());
-                RaycastHit hitInfo = new RaycastHit();
-                if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, Target.Height))
+                Vector3 v3Target = Target.ClosestPoint(transform.position, KeepOnBounds, m_LocalCollider);
+                if (XZOnly)
+                    v3Target.y = transform.position.y;
+                else
                 {
-                    v3Target.y += Target.Height - hitInfo.distance;
+                    RaycastHit hitInfo = new RaycastHit();
+                    if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, Target.Height))
+                    {
+                        v3Target.y += Target.Height - hitInfo.distance;
+                    }
                 }
 
                 transform.position = Vector3.SmoothDamp(transform.position, v3Target, ref m_Vel, FollowTime);
