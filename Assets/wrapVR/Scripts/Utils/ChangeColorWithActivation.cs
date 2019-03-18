051abea4c6569d2520a903dc19a58eb3eacd008a
John Joseph <mynameisjohnj@gmail.com>
2019-03-17 23:53:56 -0700

    Renaming this script, making test script activation customizable.

--- Assets/wrapVR/Scripts/Utils/ChangeColorWithActivation.cs
+++ Assets/wrapVR/Scripts/Utils/ChangeColorWithActivation.cs
@@ -6,15 +6,17 @@ namespace wrapVR
 {
     [RequireComponent(typeof(MeshRenderer))]
     [RequireComponent(typeof(VRInteractiveItem))]
-    public class ChangeColorOnTrigger : MonoBehaviour
+    public class ChangeColorWithActivation : MonoBehaviour
     {
+        public EActivation _Activation;
+
         // Use this for initialization
         void Start()
         {
-            GetComponent<VRInteractiveItem>().OnTriggerOver += (VRRayCaster rc) => 
+            GetComponent<VRInteractiveItem>().ActivationOverCallback(_Activation, (VRRayCaster rc) =>
             {
                 GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
-            };
+            });
         }
     }
 }
