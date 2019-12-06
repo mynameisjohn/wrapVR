0484f6318938fc051df98af3aea7d5f79bf9dffb
John Joseph <mynameisjohnj@gmail.com>
2019-12-05 23:01:07 -0500

    Making a new GearVRControllerRenderer script and applying it to the OVRController prefab.

---
+++ Assets/wrapVR/Scripts/SDK_Inputs/ControllerObjects/GearVRControllerRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wrapVR
+{
+    public class GearVRControllerRenderer : InputControllerRenderers
+    {
+        protected override void init()
+        {
+            buttonBack = transform.Find("back_button_PLY").GetComponent<Renderer>();
+            buttonHome = transform.Find("home_button_PLY").GetComponent<Renderer>();
+            trigger = transform.Find("trigger_PLY").GetComponent<Renderer>();
+            touchPad = transform.Find("disc_button_PLY").GetComponent<Renderer>();
+            controllerBody = transform.Find("chassis_PLY").GetComponent<Renderer>();
+        }
+    }
+}
