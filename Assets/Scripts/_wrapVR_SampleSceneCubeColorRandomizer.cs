f7d07d5da9ccca9468af5975bd069900b41dea50
John Joseph <mynameisjohnj@gmail.com>
2019-12-07 22:06:02 -0500

    Making a raycast teleport demo scene.

---
+++ Assets/Scripts/_wrapVR_SampleSceneCubeColorRandomizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wrapVR
+{
+    namespace Samples
+    {
+        public class _wrapVR_SampleSceneCubeColorRandomizer : MonoBehaviour
+        {
+            private void Start()
+            {
+                GetComponent<Renderer>().material.color = Random.ColorHSV();
+            }
+        }
+    }
+}
