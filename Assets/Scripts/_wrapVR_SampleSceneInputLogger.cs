df500d6f9095f09e734586a3bac09164d2c10053
John Joseph <mynameisjohnj@gmail.com>
2019-12-07 19:35:09 -0500

    Adding new demo scripts.

---
+++ Assets/Scripts/_wrapVR_SampleSceneInputLogger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wrapVR
+{
+    namespace Samples
+    {
+        public class _wrapVR_SampleSceneInputLogger : MonoBehaviour
+        {
+            [Tooltip("The activations to test; NONE will have no effect")]
+            public List<EActivation> _ActivationsToTest = new List<EActivation> { EActivation.TRIGGER };
+
+            void logInputActivationDown(VRInput input)
+            {
+                Debug.Log("VRInput " + input.Type + " is down");
+            }
+
+            void Start()
+            {
+                foreach (var input in VRCapabilityManager.inputs)
+                {
+                    foreach (var activation in _ActivationsToTest)
+                    {
+                        input.ActivationDownCallback(activation, logInputActivationDown);
+                    }
+                }
+            }
+        }
+    }
+}
